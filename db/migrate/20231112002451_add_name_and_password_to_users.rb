e8e2143e0f857fceb00f97857a3dde509d1c0446
iso-yuki <issomario1000@gmail.com>
2023-11-12 12:35:38 +0900

    merge blog

--- db/migrate/20231112002451_add_name_and_password_to_users.rb
+++ db/migrate/20231112002451_add_name_and_password_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameAndPasswordToUsers < ActiveRecord::Migration[6.1]
   def change
-    # add_column :users, :name, :string
+    add_column :users, :name, :string
     add_column :users, :password_digest, :string
   end
 end
