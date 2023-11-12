d44d59c2d32266469c2cf8db91e8e72ce8e30b8b
iso-yuki <issomario1000@gmail.com>
2023-11-12 09:43:51 +0900

    login機能を追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+# app/controllers/sessions_controller.rb
+
+class SessionsController < ApplicationController
+  include SessionsHelper
+  def new
+    # ログインフォームを表示するだけなので、何も処理は不要
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    # redirect_to calendars_path, notice: 'ログインしました。'
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to calendars_path, notice: 'ログインしました。'
+    else
+      flash.now[:alert] = '入力が正しくありません。'
+      render 'new'
+    end
+  end
+  
+  
+
+  def destroy
+    log_out
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+end
